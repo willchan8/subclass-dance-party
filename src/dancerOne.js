5eba7ad27338b9909c846ad45cbbc16ca8a849d7
willchan8 <wchan1992@gmail.com>
2019-04-15 21:28:40 -0700

    Implement two additional dancers

--- src/dancerOne.js
+++ src/dancerOne.js
@@ -1,3 +1,25 @@
-var DancerOne = function(top, left, timeBetweenSteps) {
+var dancerOne = function(top, left, timeBetweenSteps) {
+  this.oldStep = Dancer.prototype.step;
   
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  console.log(this);
+  this.$node.addClass('dancerOne');
+  this.$node.removeClass('Dancer');
+
+
+};
+
+dancerOne.prototype = Object.create(Dancer.prototype);
+dancerOne.prototype.constructor = dancerOne;
+
+
+dancerOne.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.fadeIn('slow');
+  this.$node.fadeOut('slow');
 };
--- src/dancerTwo.js
+++ src/dancerTwo.js
@@ -1,3 +1,24 @@
 var DancerTwo = function(top, left, timeBetweenSteps) {
+  this.oldStep = Dancer.prototype.step;
   
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  console.log(this);
+  this.$node.addClass('DancerTwo');
+  this.$node.removeClass('Dancer');
+
+
+};
+
+DancerTwo.prototype = Object.create(Dancer.prototype);
+DancerTwo.prototype.constructor = DancerTwo;
+
+
+DancerTwo.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
 };
